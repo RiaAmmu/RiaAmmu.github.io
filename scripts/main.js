0c2a43f1558d80feffe8a97df3d58b340ab18929
Ria Ammu <riaammu2002@gmail.com>
2025-02-28 16:40:46 +0530

    Removed unused files and updated index.html

--- scripts/main.js
+++ scripts/main.js
@@ -41,3 +41,23 @@ $('a.smooth-scroll')
     }
   }
 });
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+    
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
